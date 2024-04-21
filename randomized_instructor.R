0cd2741e59ceaa3467bf3ec40d0176b512bc2787
Lachlan Deer <lachlan.deer@gmail.com>
2024-04-21 18:02:42 +0200

    instructor code

--- randomized_instructor.R
+++ randomized_instructor.R
@@ -3,6 +3,8 @@
 #' Code accompanying Lecture 3: Causation and Randomization
 #' of Social Media and Web Analytics
 #' at TiSEM in 2024
+#' 
+#' Instructor Version
 
 # --- Load libraries --- #
 library(readr)
@@ -34,7 +36,7 @@ ggdag(dag,
                    ) + 
     theme_dag_blank()
 
-# --- Selection Effect DAG --- #
+# --- Selection Bias DAG --- #
 dag <- dagify(y ~ x,
               y ~~ z,
   x ~~ z,
@@ -67,61 +69,85 @@ charity <-
 
 head(charity, n = 5)
 
+
 # --- Plot Clickthrough --- #
-YOURCODE %>% 
+charity %>% 
+    group_by(condition) %>%
+    summarize(click = mean(clickthrough),
+              std_err = sd(clickthrough) / sqrt(n())
+              )%>%
     ggplot() +
-    geom_bar(aes(x = YOURCODE, 
-                 y = YOURCODE
-                 ), 
+    geom_bar(aes(x = condition, 
+                 y = click), 
              stat = "identity", 
              fill = "skyblue", 
-             alpha = 0.7) +
+             alpha=0.7) +
     geom_errorbar(aes(x = condition, 
-                      ymin = YOURCODE, 
-                      ymax = YOURCODE), 
+                      ymin = click-std_err, 
+                      ymax = click+std_err), 
                   width = 0.4, 
                   colour = "orange", 
                   alpha = 0.9, 
                   size = 1.5) +
     scale_y_continuous(limits=c(.13,.155), 
-                       oob = scales::squish
-                       ) +
+                       oob = scales::squish) +
     theme_bw() + 
     ggtitle("Clickthrough Rate") +
-    theme(text = element_text(size=28),
+    theme(text = element_text(size = 28),
           plot.title = element_text(hjust = 0.5))
 
-# --- Plot Recruitment --- #
 
-YOURCODE %>%
+charity %>% 
+    group_by(condition) %>%
+    summarize(recruit = mean(recruited),
+              std_err = sd(clickthrough) / sqrt(n())
+              )%>%
     ggplot() +
-    ggplot() +
-    geom_bar(aes(x = YOURCODE, 
-                 y = YOURCODE
-    ), 
-    stat = "identity", 
-    fill = "skyblue", 
-    alpha = 0.7) +
+    geom_bar(aes(x = condition,
+                 y = recruit), 
+             stat = "identity", 
+             fill = "skyblue", 
+             alpha = 0.7) +
     geom_errorbar(aes(x = condition, 
-                      ymin = YOURCODE, 
-                      ymax = YOURCODE), 
-                  width = 0.4, 
-                  colour = "orange", 
-                  alpha = 0.9, 
-                  size = 1.5) +
-    scale_y_continuous(limits=c(.025,.035), oob = scales::squish) +
+                      ymin = recruit - std_err, 
+                      ymax = recruit + std_err), 
+                  width=0.4, 
+                  colour="orange", 
+                  alpha=0.9, 
+                  size=1.5) +
+    scale_y_continuous(limits=c(.025,.035), 
+                       oob = scales::squish) +
     theme_bw() +
     ggtitle("Recruitment Rate") +
-    theme(text = element_text(size=28),
+    theme(text = element_text(size = 28),
           plot.title = element_text(hjust = 0.5))
 
-# --- Statistical Tests --- # 
-# YOUR CODE HERE
+# --- Statistical Tests ---# 
+charity %>% 
+    group_by(condition) %>%
+    summarize(click = mean(clickthrough),
+              recruit = mean(recruited))
 
+charity <-
+    charity %>%
+    mutate(clickthrough = as.logical(clickthrough),
+           recruited = as.logical(recruited)
+    )
 
-# --- Regression Based Inference --- # 
-# YOUR CODE HERE
+# clickthru
+prop_test(charity,
+          clickthrough ~ condition
+          )
 
+# recruit
+prop_test(charity,
+          recruited ~ condition
+          )
 
+# --- Regression Based Inference --- #
+clicks <- lm(clickthrough ~ condition, data = charity)
+tidy(clicks)
 
+recruit <- lm(recruited ~ condition, data = charity)
+tidy(recruit)
 
